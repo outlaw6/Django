1e76d5d38847659fb894e1116a12570bfc786a32
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-04-05 18:30:34 +0200

    Database

--- 2-Django-Project/FORMS/app/admin.py
+++ 2-Django-Project/FORMS/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import WebPage, Topic, AccessRecord
 
 # Register your models here.
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(WebPage)
