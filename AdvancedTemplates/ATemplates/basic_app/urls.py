8e0ea8b15621a8136f4d59341926964fb0dee20c
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-04-10 15:28:51 +0200

    Relative Tagging in Templates

---
+++ AdvancedTemplates/ATemplates/basic_app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from basic_app import views
+# TEMPLATE TAGGING
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('relative/', views.relative, name='relative'),
+    path('other/', views.other, name='other')
+]
