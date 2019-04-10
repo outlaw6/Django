04d2b5a84f282d6602d76673d943141df00f4b7b
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-04-10 18:08:14 +0200

    Template Inheritance with Nav

--- AdvancedTemplates/ATemplates/basic_app/urls.py
+++ AdvancedTemplates/ATemplates/basic_app/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.urls import path
 from basic_app import views
 # TEMPLATE TAGGING
 app_name = 'basic_app'
 
 urlpatterns = [
-    path('relative/', views.relative, name='relative'),
-    path('other/', views.other, name='other')
+    url('relative/', views.relative, name='relative'),
+    url('other/', views.other, name='other')
 ]
