ab713f36f8d2d173177b0ca7f57500d4207ade99
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-04-11 15:19:09 +0200

    Backend of reg

---
+++ RegisteringUsers/UserRegistration/userauth/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from userauth.models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password') #these are the fields for the forms
+                                                   # we wont take First and Last name fields
+
+class UserProfileInfo(forms.ModelForm):
+
+    class Meta():
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic')                                                   
