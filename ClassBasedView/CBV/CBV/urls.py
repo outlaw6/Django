7024203f57c8e44bad1921393e4e3e2ada60df0a
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-04-12 11:57:10 +0200

    CBV

--- ClassBasedView/CBV/CBV/urls.py
+++ ClassBasedView/CBV/CBV/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from HelloWorld import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    
+    #path('', views.index, name='index'),
+    pat('', views.CBView.as_view())
+
 ]
