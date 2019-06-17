2cf3594dd5d2250b4ad82dca513a7b6751fa4df8
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-06-17 19:16:21 +0200

    Url patterns done

--- CloneWeb/blog/models.py
+++ CloneWeb/blog/models.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 # Post class
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
