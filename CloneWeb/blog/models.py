ba991cb7ce67ab873810d66c8b50cdfa55ba5606
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-06-17 18:19:24 +0200

    Base added

--- CloneWeb/blog/models.py
+++ CloneWeb/blog/models.py
@@ -16,7 +16,28 @@ class Post(models.Model):
         self.save()
 
     def approve_comments(self):
-        return self.comments.filter(approved_comments=True)
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk }) #after I create a post
+                                                                # go to the post that is created
 
     def __str__(self):
-        return self.title 
+        return self.title
+
+class Comments(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(defualt=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
