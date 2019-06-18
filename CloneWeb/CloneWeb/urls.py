6cc5826788c6c3637d8fa72d39ae901a6060dcb8
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-06-18 14:43:06 +0200

    Editor

--- CloneWeb/CloneWeb/urls.py
+++ CloneWeb/CloneWeb/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog.ulrs
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogs.ur'))
+    path('', include('blog.urls')),
+    path('account/login/', views.login, name='login'),
+    path('account/logout/', views.logout, name='logout', kwards={'next_page': '/'}), 
 ]
