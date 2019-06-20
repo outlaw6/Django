25a214e2250788f374d5d48bf4008aaaaca8dec6
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-06-20 17:46:01 +0200

    Posts & Groups

--- SoClone/SoClone/views.py
+++ SoClone/SoClone/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import TemplateView
 
+class TestPage(TemplateView):
+    template_name = 'test.html'
 
-class HomePage(TemplateView):
-    template_name = 'index.html'
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
 
     
+class HomePage(TemplateView):
+    template_name = 'index.html'
--- SoClone/accounts/models.py
+++ SoClone/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib import auth
 
 # Create your models here.
-clas User(auth.models.User, auth.models.PermissionsMixin):
+class User(auth.models.User, auth.models.PermissionsMixin):
 
 
     def __str__(self):
