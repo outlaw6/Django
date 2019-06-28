6cb2ff4abb62b4f046e734ed18869d89a2cffff2
outlaw6 <milan.damjanovic.mbs@gmail.com>
2019-06-28 15:22:37 +0200

    Posts Groups work

--- SoClone/posts/views.py
+++ SoClone/posts/views.py
@@ -21,16 +21,13 @@ class UserPosts(generic.ListView):
     model = models.Post
     template_name = 'posts/user_post_list.html'
 
-
-
     def get_queryset(self):
         try:
-            self.post.user = User.objects.prefetch_related('posts').get(username_iexact=self.kwargs.get('username'))
+            self.post_user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
         except User.DoesNotExist:
-
             raise Http404
         else:
-            return self.post_user.post.all()
+            return self.post_user.posts.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -48,7 +45,7 @@ class PostDetail(SelectRelatedMixin, generic.DeleteView):
 class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
 
     fields = ('message', 'group')
-
+    success_url = reverse_lazy('posts:all')
     model = models.Post
 
     def form_valid(self, form):
